c165f706ab5410168c03774d87ee9f782a12d330
Florian Fittkau <ffi@informatik.uni-kiel.de>
2014-05-07 19:37:06 +0200

    3d print button and back to landscape button

---
+++ src/explorviz/visualization/main/JSHelpers.java
@@ -0,0 +1,23 @@
+package explorviz.visualization.main;
+
+public class JSHelpers {
+
+	public static native void showElementById(String id) /*-{
+		$wnd.jQuery("#" + id).show();
+	}-*/;
+
+	public static native void hideElementById(String id) /*-{
+		$wnd.jQuery("#" + id).hide();
+	}-*/;
+
+	public static native void downloadAsFile(String filename, String content) /*-{
+		var a = document.createElement('a');
+		a.href = 'data:attachment/plain,' + content;
+		a.target = '_blank';
+		a.download = filename;
+
+		$doc.body.appendChild(a);
+		a.click();
+		$doc.body.removeChild(a);
+	}-*/;
+}
