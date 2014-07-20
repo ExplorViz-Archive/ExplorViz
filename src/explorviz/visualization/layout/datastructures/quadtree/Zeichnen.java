c5a5f4585c591687cd1902fee7c0ef2e5cb82ee2
Alexander Barbie <aabar@informatik.uni-kiel.de>
2014-07-20 11:52:53 +0200

    running Version of the test Quadtree

--- src/explorviz/visualization/layout/datastructures/quadtree/Zeichnen.java
+++ src/explorviz/visualization/layout/datastructures/quadtree/Zeichnen.java
@@ -5,16 +5,26 @@ import java.awt.geom.Rectangle2D;
 import java.util.*;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Zeichnen extends JFrame {
+public class Zeichnen extends JPanel {
 	private final ArrayList<Rectangle2D> objects;
+	JPanel panel;
 
 	public Zeichnen(final QuadTree tree) {
-		setSize((int) tree.getBounds().getWidth(), (int) tree.getBounds().getHeight());
-		setVisible(true);
+		final JFrame frame = new JFrame();
+		frame.setSize((int) tree.getBounds().getWidth(), (int) tree.getBounds().getHeight());
+		frame.setVisible(true);
+		frame.setContentPane(this);
 		objects = tree.getObjectsBuh(tree);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		paintMist(getGraphics());
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		this.setSize((int) tree.getBounds().getWidth(), (int) tree.getBounds().getHeight());
+	}
+
+	@Override
+	public void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+		paintMist(g);
 	}
 
 	private ArrayList<Rectangle2D> getobjects() {
@@ -35,8 +45,15 @@ public class Zeichnen extends JFrame {
 		liste.add(new Rectangle(new Dimension(50, 50)));
 		liste.add(new Rectangle(new Dimension(75, 75)));
 		liste.add(new Rectangle(new Dimension(100, 100)));
+		liste.add(new Rectangle(new Dimension(300, 300)));
 		liste.add(new Rectangle(new Dimension(100, 100)));
-		liste.add(new Rectangle(new Dimension(100, 100)));
+		liste.add(new Rectangle(new Dimension(150, 150)));
+		liste.add(new Rectangle(new Dimension(10, 10)));
+		liste.add(new Rectangle(new Dimension(10, 10)));
+		liste.add(new Rectangle(new Dimension(40, 40)));
+		liste.add(new Rectangle(new Dimension(10, 10)));
+		liste.add(new Rectangle(new Dimension(10, 10)));
+		liste.add(new Rectangle(new Dimension(30, 30)));
 		liste.add(new Rectangle(new Dimension(10, 10)));
 		liste.add(new Rectangle(new Dimension(10, 10)));
 		liste.add(new Rectangle(new Dimension(30, 30)));
@@ -65,13 +82,15 @@ public class Zeichnen extends JFrame {
 		if (helper.biggest(liste).getWidth() >= helper.biggest(liste).getHeight()) {
 			if (size < (2 * helper.biggest(liste).getWidth())) {
 				size = 2 * helper.biggest(liste).getWidth();
+				System.out.println("Ich bin jetzt");
 			}
 		} else {
 			if (size < (2 * helper.biggest(liste).getHeight())) {
 				size = 2 * helper.biggest(liste).getHeight();
+				System.out.println("Ich bin hier");
 			}
 		}
-
+		System.out.println("size " + size);
 		final QuadTree quad = new QuadTree(0, new Rectangle(0, 0, (int) size, (int) size));
 		for (final Rectangle2D rect : liste) {
 			quad.insert(quad, rect);
