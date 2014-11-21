cd5498b12391ac8bafd95eb0bf0e1e1cc6b3ea24
Florian Fittkau <ffi@informatik.uni-kiel.de>
2014-11-21 21:49:32 +0100

    RGB color for diagnosis perspective

--- src/explorviz/plugin/attributes/IPluginKeys.java
+++ src/explorviz/plugin/attributes/IPluginKeys.java
@@ -1,6 +1,6 @@
 package explorviz.plugin.attributes;
 
-public class IPluginKeys {
+public interface IPluginKeys {
 	public static String WARNING_ANOMALY = "WARNING_ANOMALY";
 	public static String ERROR_ANOMALY = "ERROR_ANOMALY";
 
@@ -8,8 +8,7 @@ public class IPluginKeys {
 	public static String TIMESTAMP_TO_PREDICTED_RESPONSE_TIME = "TIMESTAMP_TO_PREDICTED_RESPONSE_TIME";
 	public static String TIMESTAMP_TO_ANOMALY_SCORE = "TIMESTAMP_TO_ANOMALY_SCORE";
 
-	public static String WARNING_ROOTCAUSE = "WARNING_ROOTCAUSE";
-	public static String ERROR_ROOTCAUSE = "ERROR_ROOTCAUSE";
+	public static String ROOTCAUSE_RGB_INDICATOR = "ROOTCAUSE_RGB_INDICATOR";
 
 	public static String CAPMAN_CPU_UTIL_HISTORY = "CAPMAN_CPU_UTIL_HISTORY";
 
