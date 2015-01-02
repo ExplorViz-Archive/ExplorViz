c10a4f34250bae4438603552b0bb9dcfcd41ae53
Daniel Jähde <dtj@informatik.uni-kiel.de>
2015-01-02 18:36:48 +0100

    method for calling cardinality, response time modifier added

--- src/explorviz/visualization/performanceanalysis/PerformanceAnalysis.xtend
+++ src/explorviz/visualization/performanceanalysis/PerformanceAnalysis.xtend
@@ -3,8 +3,11 @@ package explorviz.visualization.performanceanalysis
 import explorviz.shared.model.Application
 import explorviz.visualization.engine.main.SceneDrawer
 import explorviz.visualization.landscapeexchange.LandscapeExchangeManager
+import java.util.logging.Logger
 
 class PerformanceAnalysis {
+	private static final Logger log = Logger.getLogger( "Debug");
+	
 	def static void openDialog(String applicationName) {
 		if (!LandscapeExchangeManager::isStopped()) {
 			LandscapeExchangeManager::stopAutomaticExchange(System::currentTimeMillis().toString())
@@ -13,9 +16,9 @@ class PerformanceAnalysis {
 		PerformanceAnalysisJS::showDialog(applicationName)
 	}
 
-	def static void showOnlyCommunicationsAbove100ms() {
+/* 	def static void showOnlyCommunicationsAbove100ms() {
 		val application = SceneDrawer::lastViewedApplication
-
+		log.info("100ms called");
 		if (application != null) {
 			for (commu : application.communications) {
 				commu.hidden = true
@@ -25,10 +28,34 @@ class PerformanceAnalysis {
 					}
 				}
 			}
-
 			refreshView(application)
 		}
 	}
+*/	
+	def int getCallingCardinalityForMethod() {
+		val application = SceneDrawer::lastViewedApplication
+		
+		if (application != null) {
+			return application.getIncomingCommunications.size();
+		} 
+	}
+	
+	def static void showOnlyCommunicationsAboveXms(int responseTime) {
+		val application = SceneDrawer::lastViewedApplication
+		log.info("Uebergebener Wert: "+responseTime.toString());
+		if (application != null) {
+			for (commu : application.communications) {
+				commu.hidden = true
+				for (runtime : commu.traceIdToRuntimeMap.values) {
+					if (toMillis(runtime.getAverageResponseTimeInNanoSec) > responseTime) {
+						commu.hidden = false
+					}
+				}
+			}
+			refreshView(application)
+		}
+	}
+	
 	
 	def static float toMillis(float f) {
 		f / (1000 * 1000)
--- src/explorviz/visualization/performanceanalysis/PerformanceAnalysisJS.java
+++ src/explorviz/visualization/performanceanalysis/PerformanceAnalysisJS.java
@@ -1,6 +1,8 @@
 package explorviz.visualization.performanceanalysis;
 
 public class PerformanceAnalysisJS {
+	// private static final Logger log = Logger.getLogger("JSDebug");
+
 	public static native void showDialog(String applicationName) /*-{
 		$wnd.jQuery("#performanceAnalysisDialog").show();
 		$wnd.jQuery("#performanceAnalysisDialog").dialog({
@@ -20,17 +22,33 @@ public class PerformanceAnalysisJS {
 		@explorviz.visualization.engine.popover.PopoverService::hidePopover()();
 		@explorviz.visualization.engine.navigation.Navigation::cancelTimers()();
 
-		showOnlyCommunicationsAbove100msec = '<tr><th>Show only commu above 100msec:</th><td><input type="checkbox" id="showOnlyCommunicationsAbove100msec" name="showOnlyCommunicationsAbove100msec" value="showOnlyCommunicationsAbove100msec"></td></tr>'
+		//showOnlyCommunicationsAbove100msec = '<tr><th>Show only commu above 100msec:</th><td></td><td><input type="checkbox" id="showOnlyCommunicationsAbove100msec" name="showOnlyCommunicationsAbove100msec" value="showOnlyCommunicationsAbove100msec"></td></tr>'
+		showOnlyCommunicationsAboveXmsec = '<tr><th>Show only commu above Xmsec:</th><td><input type="text" id="comoverXmsec" name="comoverXmsec" value="0"</td><td><input type="checkbox" id="showOnlyCommunicationsAboveXmsec" name="showOnlyCommunicationsAboveXmsec" value="showOnlyCommunicationsAboveXmsec"></td></tr>'
 
 		$doc.getElementById("performanceAnalysisDialog").innerHTML = '<table>'
-				+ showOnlyCommunicationsAbove100msec + '</table>'
+		//+ showOnlyCommunicationsAbove100msec
+		+ showOnlyCommunicationsAboveXmsec + '</table>'
+
+		//		$wnd
+		//						.jQuery("#showOnlyCommunicationsAbove100msec")
+		//						.change(
+		//								function() {
+		//									if (this.checked) {
+		//										console.log("TEST");
+		//										@explorviz.visualization.performanceanalysis.PerformanceAnalysis::showOnlyCommunicationsAbove100ms()();
+		//									} else {
+		//										@explorviz.visualization.performanceanalysis.PerformanceAnalysis::showAllCommunications()();
+		//									}
+		//								});
 
 		$wnd
-				.jQuery("#showOnlyCommunicationsAbove100msec")
+				.jQuery("#showOnlyCommunicationsAboveXmsec")
 				.change(
 						function() {
 							if (this.checked) {
-								@explorviz.visualization.performanceanalysis.PerformanceAnalysis::showOnlyCommunicationsAbove100ms()();
+								var inputValue = parseInt($wnd.jQuery(
+										"#comoverXmsec").val());
+								@explorviz.visualization.performanceanalysis.PerformanceAnalysis::showOnlyCommunicationsAboveXms(I)(inputValue);
 							} else {
 								@explorviz.visualization.performanceanalysis.PerformanceAnalysis::showAllCommunications()();
 							}
