6ff7c09943fe8b44c3a6f6484334fd0aff169310
Enno Schwanke <ems@informatik.uni-kiel.de>
2015-01-08 16:10:49 +0100

    Implemented AbstractForecaster and NaiveForecaster.

--- src/explorviz/plugin/anomalydetection/forecast/ARIMAForecaster.java
+++ src/explorviz/plugin/anomalydetection/forecast/ARIMAForecaster.java
@@ -3,5 +3,7 @@ package explorviz.plugin.anomalydetection.forecast;
 public class ARIMAForecaster extends AbstractForecaster {
 
 	// TODO: To be implemented
-
+	public static double forecast(final double[] historyResponseTime) {
+		return 0;
+	}
 }
--- src/explorviz/plugin/anomalydetection/forecast/NaiveForecaster.java
+++ src/explorviz/plugin/anomalydetection/forecast/NaiveForecaster.java
@@ -1,7 +1,12 @@
 package explorviz.plugin.anomalydetection.forecast;
 
+import explorviz.plugin.attributes.TreeMapLongDoubleIValue;
+
 public class NaiveForecaster extends AbstractForecaster {
 
-	// TODO: To be implemented.
-
+	// TODO: To be implemented
+	public static double forecast(final TreeMapLongDoubleIValue historyResponseTime) {
+		final long key = 15;
+		return historyResponseTime.get(key);
+	}
 }
