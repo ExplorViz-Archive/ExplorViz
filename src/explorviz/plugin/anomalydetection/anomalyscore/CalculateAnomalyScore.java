ac74b97b6462f0796c99b06281bf638ca53d7cc2
Kim Mannstedt <kcm@informatik.uni-kiel.de>
2015-01-08 15:43:39 +0100

    implement anomaly score algorithm and normalize algorithm

--- src/explorviz/plugin/anomalydetection/anomalyscore/CalculateAnomalyScore.java
+++ src/explorviz/plugin/anomalydetection/anomalyscore/CalculateAnomalyScore.java
@@ -3,12 +3,11 @@ package explorviz.plugin.anomalydetection.anomalyscore;
 public class CalculateAnomalyScore {
 
 	public double getAnomalyScore(final double responseTime, final double forecastResponseTime) {
-		final double score = 0;
-
-		// TODO calculate anomaly score
+		final double score = Math.sqrt(Math.pow((responseTime - forecastResponseTime), 2));
 
 		final NormalizeAnomalyScore normalize = new NormalizeAnomalyScore();
-		final double normalizedScore = normalize.normalizeAnomalyScore(score);
+		final double normalizedScore = normalize.normalizeAnomalyScore(score, responseTime,
+				forecastResponseTime);
 
 		return normalizedScore;
 	}
