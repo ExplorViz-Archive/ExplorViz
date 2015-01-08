3364e7c642cec343a4f102636aadda8f7558b93b
Enno Schwanke <ems@informatik.uni-kiel.de>
2015-01-08 16:10:49 +0100

    Implemented AbstractForecaster and NaiveForecaster.

--- src/explorviz/plugin/anomalydetection/forecast/AbstractForecaster.java
+++ src/explorviz/plugin/anomalydetection/forecast/AbstractForecaster.java
@@ -1,9 +1,31 @@
 package explorviz.plugin.anomalydetection.forecast;
 
-public abstract class AbstractForecaster {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	// TODO: To be implemented
-	public double forecast(final double[] historyResponseTime) {
-		return 0f;
+import explorviz.plugin.anomalydetection.Configuration;
+import explorviz.plugin.attributes.TreeMapLongDoubleIValue;
+
+public abstract class AbstractForecaster {
+	private static final Logger logger = Logger.getLogger(AbstractForecaster.class.getName());
+
+	public static double forecast(final TreeMapLongDoubleIValue historyResponseTime) {
+		try {
+			switch (Configuration.FORECASTING_ALGORITHM) {
+				case "explorviz.plugin.anomalydetection.forecast.NaiveForecaster":
+					final NaiveForecaster naiveForecaster = new NaiveForecaster();
+					return naiveForecaster.forecast(historyResponseTime);
+				case "explorviz.plugin.anomalydetection.forecast.ARIMAForecaster":
+					final ARIMAForecaster arimaForecaster = new ARIMAForecaster();
+					return arimaForecaster.forecast(historyResponseTime);
+				default:
+					throw new Exception("Forecaster not available. Check configuration!");
+			}
+			// TODO: AnomalyScoreException einführen.
+		} catch (final Exception e) {
+			logger.log(Level.SEVERE, "Forecaster not available. Check configuration!", e);
+		}
+		// TODO: wird das erreicht?
+		return 0;
 	}
 }
