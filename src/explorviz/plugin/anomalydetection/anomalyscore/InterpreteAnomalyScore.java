34b8b0fe320d8634c89181cc4466eaf9b782bc47
Kim Mannstedt <kcm@informatik.uni-kiel.de>
2015-01-08 15:17:50 +0100

    Benutze Wert für Warnung und Fehler aus der Configuration Klasse

--- src/explorviz/plugin/anomalydetection/anomalyscore/InterpreteAnomalyScore.java
+++ src/explorviz/plugin/anomalydetection/anomalyscore/InterpreteAnomalyScore.java
@@ -1,5 +1,7 @@
 package explorviz.plugin.anomalydetection.anomalyscore;
 
+import explorviz.plugin.anomalydetection.Configuration;
+
 public class InterpreteAnomalyScore {
 
 	public boolean[] interprete(final double anomalyScore) {
@@ -7,11 +9,11 @@ public class InterpreteAnomalyScore {
 		final boolean[] errorWarning = new boolean[] { false, false };
 
 		// TODO use config values for warning and error
-		if (anomalyScore > 0.5) {
+		if (anomalyScore > Configuration.WARNING_ANOMALY) {
 			errorWarning[0] = true;
 		}
 
-		if (anomalyScore >= 0.7) {
+		if (anomalyScore >= Configuration.ERROR_ANOMALY) {
 			errorWarning[1] = true;
 		}
 
