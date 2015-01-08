4b64fa931cb41e75fc8964e7728d0ab6c56ea462
Enno Schwanke <ems@informatik.uni-kiel.de>
2015-01-08 15:06:58 +0100

    dh

---
+++ src/explorviz/plugin/anomalydetection/forecast/ARIMAForecaster.java
@@ -0,0 +1,7 @@
+package explorviz.plugin.anomalydetection.forecast;
+
+public class ARIMAForecaster extends AbstractForecaster {
+
+	// TODO: To be implemented
+
+}
---
+++ src/explorviz/plugin/anomalydetection/forecast/AbstractForecaster.java
@@ -0,0 +1,9 @@
+package explorviz.plugin.anomalydetection.forecast;
+
+public abstract class AbstractForecaster {
+
+	// TODO: To be implemented
+	public double forecast(final double[] historyResponseTime) {
+		return 0f;
+	}
+}
