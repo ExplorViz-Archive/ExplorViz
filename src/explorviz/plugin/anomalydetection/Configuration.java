9bb1a5d2ecd6cac400cfb4b0e3b73065102d2618
Kim Mannstedt <kcm@informatik.uni-kiel.de>
2015-01-08 15:17:50 +0100

    Benutze Wert für Warnung und Fehler aus der Configuration Klasse

--- src/explorviz/plugin/anomalydetection/Configuration.java
+++ src/explorviz/plugin/anomalydetection/Configuration.java
@@ -1,8 +1,8 @@
 package explorviz.plugin.anomalydetection;
 
 public class Configuration {
-	double WARNING_ANOMALY = 0.5;
-	double ERROR_ANOMALY = 0.8;
-	int TIME_SERIES_WINDOW_SIZE = 15;
-	String FORECASTING_ALGORITHM = "explorviz.plugin.anomalydetection.forecast.NaiveForecaster";
+	public static double WARNING_ANOMALY = 0.5;
+	public static double ERROR_ANOMALY = 0.8;
+	public static int TIME_SERIES_WINDOW_SIZE = 15;
+	public static String FORECASTING_ALGORITHM = "explorviz.plugin.anomalydetection.forecast.NaiveForecaster";
 }
