633efd439345472d6b6cc7b0e2c5d309b9253cc0
Julian Gill <julian.gill@web.de>
2015-01-12 17:18:39 +0100

    Fehler behoben damit kompiliert werden kann.

--- src/explorviz/plugin/rootcausedetection/algorithm/MeshAlgorithm.java
+++ src/explorviz/plugin/rootcausedetection/algorithm/MeshAlgorithm.java
@@ -1,20 +1,28 @@
 package explorviz.plugin.rootcausedetection.algorithm;
 
 import explorviz.plugin.rootcausedetection.model.RanCorrLandscape;
+import explorviz.plugin.rootcausedetection.model.RanCorrOperation;
 
 /**
  * This class contains an elaborated algorithm to calculate RootCauseRatings. It
  * uses the data of the entire landscape to achieve this and reaches the best
  * results while somewhat sacrificing performance.
- *
+ * 
  * @author Christian Claus Wiechmann
- *
+ * 
  */
 public class MeshAlgorithm extends AbstractRanCorrAlgorithm {
 
 	@Override
 	public void calculate(final RanCorrLandscape lscp) {
 		// TODO Auto-generated method stub
+	}
+
+	// Eingefügt damit kompiliert werden kann. //jgi
+	@Override
+	public void calculate(final RanCorrOperation operation) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/explorviz/plugin/rootcausedetection/algorithm/NeighbourAlgorithm.java
+++ src/explorviz/plugin/rootcausedetection/algorithm/NeighbourAlgorithm.java
@@ -1,20 +1,28 @@
 package explorviz.plugin.rootcausedetection.algorithm;
 
 import explorviz.plugin.rootcausedetection.model.RanCorrLandscape;
+import explorviz.plugin.rootcausedetection.model.RanCorrOperation;
 
 /**
  * This class contains a simple algorithm to calculate RootCauseRatings. It uses
  * the data of all directly adjacent elements of the element the RootCauseRating
  * is calculated for.
- *
+ * 
  * @author Christian Claus Wiechmann
- *
+ * 
  */
 public class NeighbourAlgorithm extends AbstractRanCorrAlgorithm {
 
 	@Override
 	public void calculate(final RanCorrLandscape lscp) {
 		// TODO Auto-generated method stub
+	}
+
+	// Eingefügt damit kompiliert werden kann. //jgi
+	@Override
+	public void calculate(final RanCorrOperation operation) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
