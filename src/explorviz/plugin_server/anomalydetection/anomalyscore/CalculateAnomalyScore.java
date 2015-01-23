b40a07cd335a311e6a08a591da2fd43f0f2c6454
ems <ems@informatik.uni-kiel.de>
2015-01-23 12:38:56 +0100

    Wichtiges TODO hinzugefügt: Ergebnisraum von [0,1] zu [-1,1] ändern

--- src/explorviz/plugin_server/anomalydetection/anomalyscore/CalculateAnomalyScore.java
+++ src/explorviz/plugin_server/anomalydetection/anomalyscore/CalculateAnomalyScore.java
@@ -3,6 +3,7 @@ package explorviz.plugin_server.anomalydetection.anomalyscore;
 public class CalculateAnomalyScore {
 
 	public double getAnomalyScore(final double responseTime, final double forecastResponseTime) {
+		// TODO Ergebnisraum von [0,1] zu [-1,1] ändern
 		final double score = Math.sqrt(Math.pow((responseTime - forecastResponseTime), 2));
 
 		final NormalizeAnomalyScore normalize = new NormalizeAnomalyScore();
--- src/explorviz/plugin_server/anomalydetection/anomalyscore/NormalizeAnomalyScore.java
+++ src/explorviz/plugin_server/anomalydetection/anomalyscore/NormalizeAnomalyScore.java
@@ -4,7 +4,7 @@ public class NormalizeAnomalyScore {
 
 	public double normalizeAnomalyScore(final double anomalyScore, final double responseTime,
 			final double forecastResponseTime) {
-
+		// TODO Ergebnisraum von [0,1] zu [-1,1] ändern
 		final double normalizedAnomalyScore = Math.abs(anomalyScore
 				/ (responseTime + forecastResponseTime));
 
