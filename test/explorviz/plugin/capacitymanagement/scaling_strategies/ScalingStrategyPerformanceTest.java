1e5f87db50862e7c3f993e0daf95b9636a7fd402
Julian Gill <jgi@informatik.uni-kiel.de>
2015-02-05 19:49:10 +0100

    ScalingStrategyPerformance Test eingefügt.

---
+++ test/explorviz/plugin/capacitymanagement/scaling_strategies/ScalingStrategyPerformanceTest.java
@@ -0,0 +1,52 @@
+package explorviz.plugin.capacitymanagement.scaling_strategies;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import explorviz.plugin_client.attributes.IPluginKeys;
+import explorviz.plugin_server.capacitymanagement.scaling_strategies.ScalingStrategyPerformance;
+import explorviz.shared.model.*;
+import explorviz.shared.model.System;
+
+public class ScalingStrategyPerformanceTest {
+	private Landscape landscape;
+	private List<Application> applicationList;
+	private Map<Application, Integer> planMapApplication;
+
+	@Before
+	public void before() {
+		landscape = TestLandscapeBuilder.createStandardLandscape(0);
+		applicationList = new ArrayList<Application>();
+		planMapApplication = new HashMap<Application, Integer>();
+
+		for (final System system : landscape.getSystems()) {
+			for (final NodeGroup nodeGroup : system.getNodeGroups()) {
+				for (final Node node : nodeGroup.getNodes()) {
+					for (int i = 0; i < node.getApplications().size(); i++) {
+						// manipulate RCRs
+						Application currentApplication = node.getApplications().get(i);
+						currentApplication.putGenericDoubleData(
+								IPluginKeys.ROOTCAUSE_APPLICATION_PROBABILITY, 0.7 - (0.05 * i));
+						if ((0.7 - (0.05 * i)) >= 0.6) {
+							applicationList.add(currentApplication);
+						}
+					}
+				}
+			}
+		}
+		for (Application application : applicationList) {
+			planMapApplication.put(application, 1);
+		}
+	}
+
+	@Test
+	public void testAnalyzeApplications() {
+		ScalingStrategyPerformance ssp = new ScalingStrategyPerformance();
+		assertEquals(planMapApplication, ssp.analyzeApplications(landscape, applicationList));
+
+	}
+}
