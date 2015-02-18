e38b001bcc8b2bf636fce3c5074d903019632c95
Kim Mannstedt <kcm@informatik.uni-kiel.de>
2015-02-18 16:11:12 +0100

    ersten Test für OPADx geschrieben

--- test/explorviz/plugin_server/anomalydetection/TestOPADx.java
+++ test/explorviz/plugin_server/anomalydetection/TestOPADx.java
@@ -1,47 +1,60 @@
 package explorviz.plugin_server.anomalydetection;
+
 import static org.junit.Assert.*;
 
-import org.junit.*;
+import java.util.HashMap;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import explorviz.plugin_client.attributes.IPluginKeys;
 import explorviz.plugin_client.attributes.TreeMapLongDoubleIValue;
-import explorviz.plugin_server.anomalydetection.Configuration;
-import explorviz.plugin_server.anomalydetection.OPADx;
-import explorviz.shared.model.Landscape;
-import explorviz.shared.model.TestLandscapeBuilder;
+import explorviz.shared.model.*;
 
 public class TestOPADx {
 
-	private static OPADx opadx;
-	private Landscape landscape;
+	private static Landscape landscape;
+	private static CommunicationClazz commClazz;
 
 	@BeforeClass
 	public static void beforeClass() {
-		opadx = new OPADx();
-	}
-
-	@Before
-	public void before() {
-		landscape = TestLandscapeBuilder.createStandardLandscape(0);
-		// TODO create Clazz and CommunicationClazz
+		landscape = TestLandscapeBuilder.createStandardLandscape(30);
+		commClazz = landscape.getSystems().get(0).getNodeGroups().get(0).getNodes().get(0)
+				.getApplications().get(0).getCommunications().get(0);
+		HashMap<Long, RuntimeInformation> traceIdToRuntimeMap = new HashMap<Long, RuntimeInformation>();
+		for (int i = 1; i <= 5; i++) {
+			RuntimeInformation runtime = new RuntimeInformation();
+			runtime.setAverageResponseTimeInNanoSec(2);
+			traceIdToRuntimeMap.put(new Long(i), runtime);
+		}
+		TreeMapLongDoubleIValue responseTimes = new TreeMapLongDoubleIValue();
+		for (int i = 1; i <= 29; i++) {
+			responseTimes.put(new Long(i), 2.0);
+		}
+		TreeMapLongDoubleIValue predictedResponseTimes = new TreeMapLongDoubleIValue();
+		for (int i = 1; i <= 29; i++) {
+			predictedResponseTimes.put(new Long(i), 2.0);
+		}
+		TreeMapLongDoubleIValue anomalyScore = new TreeMapLongDoubleIValue();
+		for (int i = 1; i <= 29; i++) {
+			anomalyScore.put(new Long(i), 0.0);
+		}
+		commClazz.setTraceIdToRuntimeMap(traceIdToRuntimeMap);
+		commClazz.putGenericData(IPluginKeys.TIMESTAMP_TO_RESPONSE_TIME, responseTimes);
+		commClazz.putGenericData(IPluginKeys.TIMESTAMP_TO_PREDICTED_RESPONSE_TIME,
+				predictedResponseTimes);
+		commClazz.putGenericData(IPluginKeys.TIMESTAMP_TO_ANOMALY_SCORE, anomalyScore);
 	}
 
 	@Test
-	public void testDelimitTreeMap() {
-		TreeMapLongDoubleIValue map = new TreeMapLongDoubleIValue();
-		for (int i = 0; i < (Configuration.TIME_SERIES_WINDOW_SIZE + 2); i++) {
-			map.put(new Long(i), i * 2.0);
-		}
-		TreeMapLongDoubleIValue resultMap = new TreeMapLongDoubleIValue();
-		resultMap.putAll(map);
-		resultMap.remove(0L);
-		resultMap.remove(1L);
-
-		TreeMapLongDoubleIValue resultMapFromOPADx = opadx.delimitTreeMap(map);
-
-		assertEquals(resultMap.size(), resultMapFromOPADx.size());
-		for (int i = resultMapFromOPADx.size() - 1; i > 2; i--) {
-			assertEquals(resultMap.get(new Long(i)), resultMapFromOPADx.get(new Long(i)), 0);
-		}
+	public void testDoAnomalyDetection() {
+		OPADx opadx = new OPADx();
+		opadx.doAnomalyDetection(landscape);
+		boolean anomaly = commClazz.getGenericBooleanData(IPluginKeys.WARNING_ANOMALY);
+		assertFalse(anomaly);
+		boolean parentAnomaly = commClazz.getTarget().getGenericBooleanData(
+				IPluginKeys.WARNING_ANOMALY);
+		assertFalse(parentAnomaly);
 	}
 
 }
