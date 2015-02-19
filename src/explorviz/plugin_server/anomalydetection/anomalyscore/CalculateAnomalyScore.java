e89a023b58ab73a49248a947e3225aca6c1d00fb
Kim Mannstedt <kcm@informatik.uni-kiel.de>
2015-02-19 02:58:59 +0100

    javadoc für das Package anomaly score

--- src/explorviz/plugin_server/anomalydetection/anomalyscore/CalculateAnomalyScore.java
+++ src/explorviz/plugin_server/anomalydetection/anomalyscore/CalculateAnomalyScore.java
@@ -1,7 +1,24 @@
 package explorviz.plugin_server.anomalydetection.anomalyscore;
 
+/**
+ * This class calculate the anomaly score
+ *
+ * @author Kim Christian Mannstedt
+ * @author Enno Schwanke
+ *
+ */
 public class CalculateAnomalyScore {
 
+	/**
+	 *
+	 * @param responseTime
+	 *            actual response Time
+	 * @param forecastResponseTime
+	 *            forecast response time for actual response time
+	 * @return normalized anomaly score
+	 * @throws CorruptedParametersException
+	 *             throw exception if there are negative values for parameters
+	 */
 	public double getAnomalyScore(final double responseTime, final double forecastResponseTime)
 			throws CorruptedParametersException {
 		// Ergebnisraum von [0,x] zu [-x,x] geändert
@@ -9,7 +26,7 @@ public class CalculateAnomalyScore {
 		// forecastResponseTime), 2));
 		if ((responseTime < 0) || (forecastResponseTime < 0)) {
 			throw new CorruptedParametersException(
-					"Reponse time or forecast response time is less than 0. That is not possible.");
+					"Response time or forecast response time is less than 0. That is not possible.");
 		}
 		final double score = responseTime - forecastResponseTime;
 
--- src/explorviz/plugin_server/anomalydetection/anomalyscore/CorruptedParametersException.java
+++ src/explorviz/plugin_server/anomalydetection/anomalyscore/CorruptedParametersException.java
@@ -3,11 +3,23 @@ package explorviz.plugin_server.anomalydetection.anomalyscore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * This exception gets thrown if there are corrupted Parameters
+ *
+ * @author Enno Schwanke
+ *
+ */
 public class CorruptedParametersException extends RuntimeException {
 
 	private static final Logger logger = Logger.getLogger(CorruptedParametersException.class
 			.getName());
 
+	/**
+	 * Create exception with severe logginglevel
+	 *
+	 * @param message
+	 *            information message
+	 */
 	public CorruptedParametersException(String message) {
 		super(message);
 		logger.log(Level.SEVERE, message);
