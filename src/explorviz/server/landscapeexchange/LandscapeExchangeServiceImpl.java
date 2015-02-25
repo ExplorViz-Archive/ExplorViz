2335ae455a1679ec85a90c3a6581818e2cac6e70
Florian Fittkau <ffi@informatik.uni-kiel.de>
2015-02-25 10:29:35 +0100

    fixed webserver not starting

--- src/explorviz/server/landscapeexchange/LandscapeExchangeServiceImpl.java
+++ src/explorviz/server/landscapeexchange/LandscapeExchangeServiceImpl.java
@@ -12,7 +12,7 @@ import explorviz.shared.model.Landscape;
 import explorviz.visualization.landscapeexchange.LandscapeExchangeService;
 
 public class LandscapeExchangeServiceImpl extends RemoteServiceServlet implements
-		LandscapeExchangeService {
+LandscapeExchangeService {
 
 	private static final long serialVersionUID = 4310863128987822861L;
 	private static LandscapeRepositoryModel model;
@@ -60,6 +60,12 @@ public class LandscapeExchangeServiceImpl extends RemoteServiceServlet implement
 
 	private static void startRepository() {
 		model = new LandscapeRepositoryModel();
-		new RepositoryStarter().start(model);
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				new RepositoryStarter().start(model);
+			}
+		}).start();
 	}
 }
