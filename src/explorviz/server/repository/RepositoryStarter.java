ee0c024d977200cce9cf8a6a8c0e0a36423bbad7
Florian Fittkau <ffi@informatik.uni-kiel.de>
2015-05-07 11:13:49 +0200

    minor

--- src/explorviz/server/repository/RepositoryStarter.java
+++ src/explorviz/server/repository/RepositoryStarter.java
@@ -1,22 +1,26 @@
 package explorviz.server.repository;
 
-import java.util.Queue;
-
+import explorviz.live_trace_processing.Constants;
 import explorviz.live_trace_processing.configuration.Configuration;
 import explorviz.live_trace_processing.configuration.ConfigurationFactory;
 import explorviz.live_trace_processing.filter.SinglePipeConnector;
+import explorviz.live_trace_processing.filter.counting.RecordCountingFilter;
 import explorviz.live_trace_processing.main.FilterConfiguration;
 import explorviz.live_trace_processing.record.IRecord;
 
 public class RepositoryStarter {
 	public void start(final LandscapeRepositoryModel model) {
-		final SinglePipeConnector<IRecord> modelConnector = new SinglePipeConnector<IRecord>(16);
+		final SinglePipeConnector<IRecord> modelConnector = new SinglePipeConnector<IRecord>(64);
 
 		new LandscapeRepositorySink(modelConnector, model).start();
 
-		final Queue<IRecord> sink = modelConnector.registerProducer();
+		final SinglePipeConnector<IRecord> recordCountingConnector = new SinglePipeConnector<IRecord>(
+				Constants.TRACE_SUMMARIZATION_DISRUPTOR_SIZE);
+		new RecordCountingFilter(recordCountingConnector, modelConnector.registerProducer())
+				.start();
 
 		final Configuration configuration = ConfigurationFactory.createSingletonConfiguration();
-		FilterConfiguration.configureAndStartFilters(configuration, sink);
+		FilterConfiguration.configureAndStartFilters(configuration,
+				recordCountingConnector.registerProducer());
 	}
 }
