df732b13eee88553dc9014e3a58f23c1dfac398a
Alexander Krause <akr@informatik.uni-kiel.de>
2016-08-05 11:07:02 +0200

    leap translation

--- src/explorviz/visualization/engine/main/WebVRJS.java
+++ src/explorviz/visualization/engine/main/WebVRJS.java
@@ -92,7 +92,7 @@ public class WebVRJS {
 		// detached render method
 		// to minimize delay between
 		// scene update and sending scene
-		// to HMD 
+		// to HMD
 		function render() {
 
 			if (showControllerRay) {
@@ -265,11 +265,11 @@ public class WebVRJS {
 								oldGamePadY = gamepad.axes[0];
 							}
 
-							// rotate based on trackpad					
-							renderingContext.landscape.rotation.y += gamepad.axes[0]
+							// rotate based on trackpad
+							landscape.rotation.y += gamepad.axes[0]
 									- oldGamePadY;
 
-							renderingContext.landscape.rotation.x += gamepad.axes[1]
+							landscape.rotation.x += gamepad.axes[1]
 									- oldGamePadX;
 
 							oldGamePadX = gamepad.axes[1];
@@ -369,12 +369,14 @@ public class WebVRJS {
 		}
 
 		// init leap
-		// TODO: activate leap interaction when controllers are not in view
-
 		// initializes the LEAP Motion library for gesture control
 
 		var leapController;
 		var calculateLeapRay = false;
+		var initial = true;
+
+		var currentHands = null;
+		previousHands = null;
 
 		function initLeap() {
 
@@ -394,6 +396,18 @@ public class WebVRJS {
 						});
 					} else {
 						calculateLeapRay = false;
+					}
+
+					if (frame.hands[0] != null) {
+						if (initial) {
+							currentHands = frame.hands;
+							previousHands = frame.hands;
+							initial = false;
+						} else {
+							currentHands = frame.hands;
+							previousHands = leapController.frame(1).hands;
+							gestureDetection();
+						}
 					}
 
 				} else {
@@ -416,6 +430,218 @@ public class WebVRJS {
 			//				renderer : renderingContext.renderer,
 			//				camera : renderingContext.camera
 			//			});
+		}
+
+		function gestureDetection() {
+			if (checkHands()) {
+				translation();
+				rotation();
+			}
+		}
+
+		function checkHands() {
+			var previousHandsAvail = typeof previousHands != 'undefined';
+			var currentHandsAvail = typeof currentHands != 'undefined';
+			var sameCountOfHands = previousHands.length == currentHands.length;
+			var maxTwoHands = currentHands.length == 1
+					|| currentHands.length == 2;
+			return previousHandsAvail && currentHandsAvail && sameCountOfHands
+					&& maxTwoHands;
+		}
+
+		var flags = new Array(0, 0, 0, 0, 0, 0);
+		var frameCounter = 0;
+		var anchorTransZoom;
+		var anchorRot;
+
+		function translation() {
+			var zoomIdx = 0;
+			var transIdx = 1;
+			var transZoomTimerIdx = 2;
+			if (flags[zoomIdx] > 1)
+				return;
+			currentHands
+					.forEach(function(element, index) {
+						if (element.grabStrength >= 0.95
+								&& element.type == "right") {
+							// check for: new hand in view or hand reappeared
+							// => id change => anchor reset
+							if (anchorTransZoom != null
+									&& anchorTransZoom.id != element.id) {
+								flags[transIdx] = 0;
+								flags[transZoomTimerIdx] = 0;
+							}
+							// set anchor and start timer for
+							// (non-)intentional interaction
+							if (flags[transIdx] == 0) {
+								flags[transZoomTimerIdx] = 0;
+								flags[transIdx] = 1;
+								anchorTransZoom = element;
+								if (flags[transZoomTimerIdx] == 0) {
+									flags[transZoomTimerIdx] = 1;
+									setTimeout(function() {
+										flags[transZoomTimerIdx] = 2;
+									}, 250);
+								}
+							}
+							// check if intentional
+							if (flags[transIdx] == 1) {
+								if ((Math.abs(element.palmPosition[0]
+										- anchorTransZoom.palmPosition[0]) > 0.07 || Math
+										.abs(element.palmPosition[1]
+												- anchorTransZoom.palmPosition[1]) > 0.07)
+										&& flags[transZoomTimerIdx] == 2) {
+									flags[transIdx] = 2;
+								} else {
+									if (flags[transIdx] == 2)
+										flags[transIdx] = 0;
+									return;
+								}
+							}
+							// proceed if intentional with calculation
+							if (flags[transIdx] == 2) {
+								var previousHand = leapController.frame(1)
+										.hand(anchorTransZoom.id);
+								if (previousHand == null)
+									return;
+								var movementX = (element.palmPosition[0] - previousHand.palmPosition[0]);
+								var movementY = (element.palmPosition[1] - previousHand.palmPosition[1]);
+								var movementZ = (previousHand.palmPosition[2] - element.palmPosition[2]);
+
+								landscape.position.x += movementX * 200.0;
+								landscape.position.y += movementY * 200.0;
+								landscape.position.z += movementZ * -200.0;
+								return;
+							}
+						} else if (element.type == "right") {
+							// reset
+							flags[transIdx] = 0;
+							flags[transZoomTimerIdx] = 0;
+						}
+					});
+		}
+		function rotation() {
+			var rotIdx = 4;
+			var rotTimerIdx = 5;
+			currentHands
+					.forEach(function(element, index) {
+						if (element.grabStrength >= 0.95
+								&& element.type == "left") {
+							// check for: new hand in view or hand reappeared
+							// => id change => anchor reset
+							if (anchorRot != null && anchorRot.id != element.id) {
+								flags[rotIdx] = 0;
+								flags[rotTimerIdx] = 0;
+							}
+							// set anchor and start timer for
+							// (non-)intentional interaction
+							if (flags[rotIdx] == 0) {
+								flags[rotTimerIdx] = 0;
+								flags[rotIdx] = 1;
+								anchorRot = element;
+								if (flags[rotTimerIdx] == 0) {
+									flags[rotTimerIdx] = 1;
+									setTimeout(function() {
+										flags[rotTimerIdx] = 2;
+									}, 250);
+								}
+							}
+							// check if intentional
+							if (flags[rotIdx] == 1) {
+								if ((Math.abs(element.palmPosition[0]
+										- anchorRot.palmPosition[0]) > 0.07 || Math
+										.abs(element.palmPosition[1]
+												- anchorRot.palmPosition[1]) > 0.07)
+										&& flags[rotTimerIdx] == 2) {
+									flags[rotIdx] = 2;
+								} else {
+									if (flags[rotIdx] == 2)
+										flags[rotIdx] = 0;
+									return;
+								}
+							}
+							// proceed if intentional with calculation
+							if (flags[rotIdx] == 2) {
+								var previousHand = leapController.frame(1)
+										.hand(anchorRot.id);
+								if (previousHand == null)
+									return;
+								var movementX = (element.palmPosition[0] - previousHand.palmPosition[0])
+										* (renderingContext.renderer.domElement.clientWidth);
+								var movementY = (previousHand.palmPosition[1] - element.palmPosition[1])
+										* (renderingContext.renderer.domElement.clientWidth);
+								x += movementX;
+								y += movementY;
+								@explorviz.visualization.engine.navigation.Navigation::mouseMoveVRHandler(IIZZ)(x, y, false, true);
+								return;
+							}
+						} else if (element.type == "left") {
+							// reset
+							flags[rotIdx] = 0;
+							flags[rotTimerIdx] = 0;
+						}
+					});
+		}
+		function zoom() {
+			var zoomIdx = 0;
+			var transIdx = 1;
+			var transZoomTimerIdx = 2;
+			if (flags[transIdx] > 1)
+				return;
+			currentHands
+					.forEach(function(element, index) {
+						if (element.grabStrength >= 0.95
+								&& element.type == "right") {
+							// check for: new hand in view or hand reappeared
+							// => id change => anchor reset
+							if (anchorTransZoom != null
+									&& anchorTransZoom.id != element.id) {
+								flags[transIdx] = 0;
+								flags[transZoomTimerIdx] = 0;
+							}
+							frameCounter = ++frameCounter % 6;
+							if (frameCounter != 0)
+								return;
+							// set anchor and start timer for
+							// (non-)intentional interaction
+							if (flags[zoomIdx] == 0) {
+								flags[transZoomTimerIdx] = 0;
+								flags[zoomIdx] = 1;
+								anchorTransZoom = element;
+								if (flags[transZoomTimerIdx] == 0) {
+									flags[transZoomTimerIdx] = 1;
+									setTimeout(function() {
+										flags[transZoomTimerIdx] = 2;
+									}, 250);
+								}
+							}
+							// check if intentional
+							if (flags[zoomIdx] == 1) {
+								if (Math.abs(element.palmPosition[2]
+										- anchorTransZoom.palmPosition[2]) > 0.06
+										&& flags[transZoomTimerIdx] == 2) {
+									flags[zoomIdx] = 2;
+								} else {
+									if (flags[zoomIdx] == 2)
+										flags[zoomIdx] = 0;
+									return;
+								}
+							}
+							// proceed if intentional with calculation
+							if (flags[zoomIdx] == 2) {
+								var previousHand = leapController.frame(1)
+										.hand(anchorTransZoom.id);
+								if (previousHand == null)
+									return;
+								var movementZ = (previousHand.palmPosition[2] - element.palmPosition[2]);
+								@explorviz.visualization.engine.navigation.Navigation::mouseWheelHandler(I)(movementZ);
+							}
+						} else if (element.type == "right") {
+							// reset
+							flags[zoomIdx] = 0;
+							flags[transZoomTimerIdx] = 0;
+						}
+					});
 		}
 
 	}-*/;
